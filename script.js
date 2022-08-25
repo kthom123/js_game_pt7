822ed7c6b267d4f742c34bfe7ff02612c73a0f65
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-25 17:17:30 +0900

    started js

---
+++ script.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', function(){
+  const canvas = document.getElementById('canvas7');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 800;
+  canvas.height = 720;
+
+  class InputHandler {
+    constructor(){
+      this.keys = []
+      window.addEventListener('keydown', function(e){
+        console.log(e);
+      });
+    }
+  }
+
+  class Player {
+
+  }
+
+  class Background {
+
+  }
+
+  class Enemy {
+
+  }
+
+  function handleEnemies(){
+
+  }
+
+  function displayStatusText(){
+
+  }
+
+  const input = new InputHandler();
+  function animate(){
+
+  }
+});
