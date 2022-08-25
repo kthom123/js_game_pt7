eb098ddc957afbde2b158e9395fee341ed40222b
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-25 17:30:41 +0900

    added player constructor

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ window.addEventListener('load', function(){
                 && this.keys.indexOf(e.key) === -1){
             this.keys.push(e.key);
         }
-        console.log(e.key, this.keys);
       });
       window.addEventListener('keyup', e => {
         if (    e.key === 'ArrowDown' ||
@@ -24,13 +23,23 @@ window.addEventListener('load', function(){
                 e.key === 'ArrowRight'){
            this.keys.splice(this.keys.indexOf(e.key), 1);
         }
-        console.log(e.key, this.keys);
       });
     }
   }
 
   class Player {
-
+    constructor(gameWidth, gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = 0;
+    }
+    draw(context){
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+    }
   }
 
   class Background {
@@ -50,6 +59,8 @@ window.addEventListener('load', function(){
   }
 
   const input = new InputHandler();
+  const player = new Player();
+
   function animate(){
 
   }
