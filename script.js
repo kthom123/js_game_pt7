a95ce10998effce1030448f163de2bb7af941a0a
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-26 18:22:35 +0900

    added speed

--- script.js
+++ script.js
@@ -36,14 +36,17 @@ window.addEventListener('load', function(){
       this.x = 0;
       this.y = this.gameHeight - this.height;
       this.image = document.getElementById('playerImage');
+      this.frameX = 0;
+      this.frameY = 0;
+      this.speed = 0;
     }
     draw(context){
       context.fillStyle = 'white';
       context.fillRect(this.x, this.y, this.width, this.height);
-      context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, 5 * this.width, 1 * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(){
-      this.x++;
+      this.x += this.speed;
     }
   }
 
