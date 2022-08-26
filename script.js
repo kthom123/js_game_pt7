cf20a5ac5216a92577fc8fa9443bf28d36d3cfa9
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-26 18:20:18 +0900

    edited draw function

--- script.js
+++ script.js
@@ -35,10 +35,12 @@ window.addEventListener('load', function(){
       this.height = 200;
       this.x = 0;
       this.y = this.gameHeight - this.height;
+      this.image = document.getElementById('playerImage');
     }
     draw(context){
       context.fillStyle = 'white';
       context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update(){
       this.x++;
