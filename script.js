1fa0a6d107e10b55eb19dd50c9e44fad23e6fef0
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-28 13:59:14 +0900

    added background

--- script.js
+++ script.js
@@ -65,8 +65,10 @@ window.addEventListener('load', function(){
       this.y += this.vy;
       if (!this.onGround()){
           this.vy += this.weight;
+          this.frameY = 1;
       } else {
           this.vy = 0;
+          this.frameY = 0;
       }
       if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
     }
@@ -76,6 +78,23 @@ window.addEventListener('load', function(){
   }
 
   class Background {
+    constructor(gameWidth, gameHeight){
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = document.getElementById('backgroundImage');
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 720;
+      this.speed = 20;
+    }
+    draw(context){
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+    update(){
+      this.x -= this.speed;
+      if (this.x < 0 - this.width) this.x = 0;
+    }
 
   }
 
@@ -93,9 +112,12 @@ window.addEventListener('load', function(){
 
   const input = new InputHandler();
   const player = new Player(canvas.width, canvas.height);
+  const background = new Background(canvas.width, canvas.height);
 
   function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw(ctx);
+    background.update();
     player.draw(ctx);
     player.update(input);
     requestAnimationFrame(animate);
